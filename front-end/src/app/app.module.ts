1fb5727362c9cf6761ffed8ce6572a679aa2c1c4
KareemSapi <sapikareem@gmail.com>
2022-10-06 22:27:22 +0300

    re-creating frontend app

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ThemeModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
